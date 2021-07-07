82bf4923e307a90cad47523154de9034b7a020c3
Chetan <chetankhandodiya@gmail.com>
2021-07-07 19:47:44 +0530

    My first commit

---
+++ public/script.js
@@ -0,0 +1,119 @@
+const socket = io("/");
+
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
+});
+
+const peers={}
+
+let myVideoStream;
+
+var getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia;
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });   
+  });
+
+peer.on("call", function (call) {
+  getUserMedia(
+    { video: true, audio: true },
+    function (stream) {
+      call.answer(stream); // Answer the call with an A/V stream.
+      const video = document.createElement("video");
+      call.on("stream", function (remoteStream) {
+        addVideoStream(video, remoteStream);
+      });
+    },
+    function (err) {
+      console.log("Failed to get local stream", err);
+    }
+  );
+});
+
+socket.on("user-disconnect", userId=>{
+  if(peers[userId])peers[userId].close()
+})
+
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+const connectToNewUser = (userId, streams) => {
+  var call = peer.call(userId, streams);
+  console.log(call);
+  var video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    console.log(userVideoStream);
+    addVideoStream(video, userVideoStream);
+  });
+   call.on('close',()=>{
+    video.remove()
+  })
+
+  peers[userId]=call
+};
+
+const addVideoStream = (videoEl, stream) => {
+  videoEl.srcObject = stream;
+  videoEl.addEventListener("loadedmetadata", () => {
+    videoEl.play();
+  });
+
+  videoGrid.append(videoEl);
+  let totalUsers = document.getElementsByTagName("video").length;
+  if (totalUsers > 1) {
+    for (let index = 0; index < totalUsers; index++) {
+      document.getElementsByTagName("video")[index].style.width =
+        100 / totalUsers + "%";
+    }
+  }
+};
+
+const playStop = () => {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+};
+const setPlayVideo = () => {
+  const html = `<i class="unmute fa fa-pause-circle"></i>
+  <span class="unmute">Resume Video</span>`;
+  document.getElementById("playPauseVideo").innerHTML = html;
+};
+
+const setStopVideo = () => {
+  const html = `<i class=" fa fa-video-camera"></i>
+  <span class="">Pause Video</span>`;
+  document.getElementById("playPauseVideo").innerHTML = html;
+};
